5c1f3f7def5266ae2c2789aa57f1f6b9ddc94b66
A Sorokin <lptolik@gmail.com>
2018-01-25 14:27:48 +0300

    making graph

--- R/makeGraph.R
+++ R/makeGraph.R
@@ -27,18 +27,19 @@
 #' @seealso \code{\link{getMetaColOrder}} for ordering columns.
 #'
 #' @examples
-makeMetaNodes<-function(df,colOrder,strength=1e3){
-  if(missing(colOrder)){
-    colOrder<-getMetaColOrder(df)
+makeMetaNodes <- function(df, colOrder, strength = 1e3) {
+  if (missing(colOrder)) {
+    colOrder <- getMetaColOrder(df)
   }
-  if(length(strength)==1){
-    strength<-rep(strength,length(colOrder)-1)
-  }else if(length(strength)>length(colOrder)-1){
-    strength<-strength[1:(length(colOrder)-1)]
+  if (length(strength) == 1) {
+    strength <- rep(strength, length(colOrder) - 1)
+  } else if (length(strength) > length(colOrder) - 1) {
+    strength <- strength[1:(length(colOrder) - 1)]
   }
-  v<-ldply(colOrder,makeMetaVertex,df)
-  e<-ldply(1:(length(colOrder)-1),makeMetaEdges,colOrder,df,v,strength)
-  return(list(vertex=v,edges=e))
+  v <- ldply(colOrder, makeMetaVertex, df)
+  e <-
+    ldply(1:(length(colOrder) - 1), makeMetaEdges, colOrder, df, v, strength)
+  return(list(vertex = v, edges = e))
 }
 
 #' Create metavertices from metadata column.
@@ -51,10 +52,18 @@ makeMetaNodes<-function(df,colOrder,strength=1e3){
 #'
 #' @seealso \code{\link[igraph]{graph_from_data_frame}}
 #' @examples
-makeMetaVertex<-function(column,df){
-  val<-as.character(unique(df[,column]))
-  vd<-data.frame(ID=paste0(column,seq_along(val)),
-                 Name=val,Type=column)
+makeMetaVertex <- function(column, df) {
+  val <- as.character(unique(df[, column]))
+  tp <- column
+  if (tolower(tp) == 'feature') {
+    tp <- 'cFeature'
+  }
+  vd <- data.frame(
+    ID = paste0(tp, seq_along(val)),
+    Name = val,
+    Type = tp,
+    stringsAsFactors = FALSE
+  )
   return(vd)
 }
 
@@ -71,17 +80,33 @@ makeMetaVertex<-function(column,df){
 #'
 #' @seealso \code{\link[igraph]{graph_from_data_frame}}
 #' @examples
-makeMetaEdges<-function(idx,columns,df,vertex,strength){
-  if(idx>=length(columns)){
-    warning(paste('Index is outside of the column vector:',idx,' - ',length(columns)-1,'\n'))
-    return(data.frame(Source='A',Target='B',value=-1e-3)[FALSE,])
+makeMetaEdges <- function(idx, columns, df, vertex, strength) {
+  if (idx >= length(columns)) {
+    warning(paste(
+      'Index is outside of the column vector:',
+      idx,
+      ' - ',
+      length(columns) - 1,
+      '\n'
+    ))
+    return(data.frame(
+      Source = 'A',
+      Target = 'B',
+      value = -1e-3,
+      stringsAsFactors = FALSE
+    )[FALSE, ])
   }
-  cSource<-columns[idx]
-  cTarget<-columns[idx+1]
-  sdf<-unique(df[,c(cSource,cTarget)])
-  sidx<-match(sdf[,cSource],vertex$Name)
-  tidx<-match(sdf[,cTarget],vertex$Name)
-  val<-data.frame(Source=as.character(vertex$ID[sidx]),Target=as.character(vertex$ID[tidx]),value=strength[idx])
+  cSource <- columns[idx]
+  cTarget <- columns[idx + 1]
+  sdf <- unique(df[, c(cSource, cTarget)])
+  sidx <- match(sdf[, cSource], vertex$Name)
+  tidx <- match(sdf[, cTarget], vertex$Name)
+  val <- data.frame(
+    Source = as.character(vertex$ID[sidx]),
+    Target = as.character(vertex$ID[tidx]),
+    value = strength[idx],
+    stringsAsFactors = FALSE
+  )
   return(val)
 }
 
@@ -95,12 +120,24 @@ makeMetaEdges<-function(idx,columns,df,vertex,strength){
 #' @examples
 #' md<-data.frame(class=c(rep('A',4),rep('B',4)),subclass=paste0(c(rep('A',4),rep('B',4)),rep(c(1,2),4)),sample=paste0('sample',1:8))
 #' getMetaColOrder(md)
-getMetaColOrder<-function(df){
-  t<-apply(df,2,function(.x){length(table(.x))})
-  colOrder<-names(df)[order(t,decreasing = FALSE)]
+getMetaColOrder <- function(df) {
+  t <- apply(df, 2, function(.x) {
+    length(table(.x))
+  })
+  colOrder <- names(df)[order(t, decreasing = FALSE)]
   return(colOrder)
 }
-#' Make FS graph.
+
+#' Make data.frames for FS graph.
+#'
+#'
+#' @description  \code{colnames} of \code{featureMatrix} corresponds to feature
+#' names and \code{rownames} corresponds to names of samples. If name of some
+#' row does not correspond to some value in the metadata \code{data.frame}
+#' column that correspond to the last name in \code{colOrder}, that row will be
+#' excluded from consideration. If any value in the metadata \code{data.frame}
+#' column that correspond to the last name in \code{colOrder} is not found in
+#' \code{rownames(featureMatrix)} error will be thrown.
 #'
 #' @param featureMatrix matrix of feature-sample edge strength.
 #' @param metadata metadata \code{data.frame} for metanodes and metaedges.
@@ -110,14 +147,92 @@ getMetaColOrder<-function(df){
 #' @return igraph graph object
 #' @export
 #'
-#' @import igraph
+#' @import igraph reshape2 RColorBrewer
 #' @examples
-makeGraph<-function(featureMatrix,metadata,colOrder,metaStrength=1e3){
-  if(length(metaStrength)==1){
-    metaStrength<-rep(metaStrength,length(colOrder)-1)
-  }else if(length(metaStrength)>length(colOrder)-1){
-    metaStrength<-metaStrength[1:(length(colOrder)-1)]
+makeGraphDFs <-
+  function(featureMatrix,
+           metadata,
+           colOrder,
+           metaStrength = 1e3) {
+    if (length(metaStrength) == 1) {
+      metaStrength <- rep(metaStrength, length(colOrder) - 1)
+    } else if (length(metaStrength) > length(colOrder) - 1) {
+      metaStrength <- metaStrength[1:(length(colOrder) - 1)]
+    }
+    if (missing(colOrder)) {
+      colOrder <- getMetaColOrder(metadata)
+    }
+    snames <- rownames(featureMatrix)
+    fnames <- colnames(featureMatrix)
+    sidx <- match(metadata[, rev(colOrder)[1]], snames)
+    if (any(is.na(sidx))) {
+      idx <- which(is.na(sidx))
+      stop(
+        paste(
+          "Not all samples from metadata correspond to rows in featureMatrix: (",
+          paste('"', metadata[idx, rev(colOrder)[1]], '"', collapse = ', '),
+          ')\n'
+        )
+      )
+    }
+    snames <- snames[sidx]
+    featureMatrix <- featureMatrix[sidx, ]
+    l <-
+      makeMetaNodes(df = metadata,
+                    colOrder = colOrder,
+                    strength = metaStrength)
+    v <- l$vertex
+    v <- rbind(v,
+               data.frame(
+                 ID = paste0('f', 1:dim(featureMatrix)[2]),
+                 Name = fnames,
+                 Type = 'feature',
+                 stringsAsFactors = FALSE
+               ))
+    tp <- unique(v$Type)
+    cl <- brewer.pal(length(tp), 'Set3')
+    v$color <- cl[match(v$Type, tp)]
+    v$size <- match(v$Type, rev(tp))
+
+    e <- l$edges
+    sID <- v$ID[match(snames, v$Name)]
+    rownames(featureMatrix) <- sID
+    e <- rbind(e, melt(featureMatrix, varnames = c('Source', 'Target')))
+    return(list(vertex = v, edges = e))
   }
-  g<-empty_graph()
+
+#' Make FS graph.
+#'
+#'
+#' @description  \code{colnames} of \code{featureMatrix} corresponds to feature
+#' names and \code{rownames} corresponds to names of samples. If name of some
+#' row does not correspond to some value in the metadata \code{data.frame}
+#' column that correspond to the last name in \code{colOrder}, that row will be
+#' excluded from consideration. If any value in the metadata \code{data.frame}
+#' column that correspond to the last name in \code{colOrder} is not found in
+#' \code{rownames(featureMatrix)} error will be thrown.
+#'
+#' @param featureMatrix matrix of feature-sample edge strength.
+#' @param metadata metadata \code{data.frame} for metanodes and metaedges.
+#' @param colOrder see in \code{\link{makeMetaNodes}}
+#' @param metaStrength see \code{strength} in \code{\link{makeMetaNodes}}
+#'
+#' @return igraph graph object
+#' @export
+#'
+#' @import igraph reshape2 RColorBrewer
+#' @examples
+makeGraph<-function(featureMatrix,
+                    metadata,
+                    colOrder,
+                    metaStrength = 1e3){
+  l<-makeGraphDFs(featureMatrix=featureMatrix,
+                   metadata=metadata,
+                   colOrder=colOrder,
+                   metaStrength = metaStrength)
+  g <- graph_from_data_frame(d = l$edges,
+                             directed = FALSE,
+                             vertices = l$vertex)
   return(g)
+
 }
--- tests/testthat/test_makeGraph.R
+++ tests/testthat/test_makeGraph.R
@@ -1,9 +1,17 @@
 context("MakeMetaNodes")
 library(plyr)
+library(reshape2)
+library(igraph)
+library(RColorBrewer)
 
 md<-data.frame(class=c(rep('A',4),rep('B',4)),
                subclass=paste0(c(rep('A',4),rep('B',4)),rep(c(1,2),4)),
-               sample=paste0('sample',1:8))
+               sample=paste0('sample',1:8),
+               stringsAsFactors = FALSE)
+
+fm<-matrix(1:200,nrow = 8,ncol = 25)
+colnames(fm)<-paste0('f',1:25)
+rownames(fm)<-md$sample
 
 test_that('getMetaColOrder makes proper order',{
   o<-getMetaColOrder(md)
@@ -19,7 +27,8 @@ test_that('makeMetaVertex makes proper vertex table',{
   expect_identical(v,
                    data.frame(ID=c('class1','class2'),
                               Name=c('A','B'),
-                              Type=c('class','class')))
+                              Type=c('class','class'),
+                              stringsAsFactors = FALSE))
 })
 
 test_that('makeMetaVertex makes proper vertex table for subclass',{
@@ -29,7 +38,51 @@ test_that('makeMetaVertex makes proper vertex table for subclass',{
   expect_identical(v,
                    data.frame(ID=c('subclass1','subclass2','subclass3','subclass4'),
                               Name=c('A1','A2','B1','B2'),
-                              Type=c('subclass','subclass','subclass','subclass')))
+                              Type=c('subclass','subclass','subclass','subclass'),
+                              stringsAsFactors = FALSE))
+})
+
+test_that('makeMetaVertex makes proper vertex table for "feature" column',{
+  md1<-md
+  names(md1)[2]<-'feature'
+  v<-makeMetaVertex('feature',md1)
+  expect_s3_class(v,'data.frame')
+  expect_equal(dim(v),c(4,3))
+  expect_identical(v,
+                   data.frame(ID=c('cFeature1','cFeature2','cFeature3','cFeature4'),
+                              Name=c('A1','A2','B1','B2'),
+                              Type=c('cFeature','cFeature','cFeature','cFeature'),
+                              stringsAsFactors = FALSE))
+  md2<-md
+  names(md2)[2]<-'FEATURE'
+  v<-makeMetaVertex('FEATURE',md2)
+  expect_s3_class(v,'data.frame')
+  expect_equal(dim(v),c(4,3))
+  expect_identical(v,
+                   data.frame(ID=c('cFeature1','cFeature2','cFeature3','cFeature4'),
+                              Name=c('A1','A2','B1','B2'),
+                              Type=c('cFeature','cFeature','cFeature','cFeature'),
+                              stringsAsFactors = FALSE))
+  md3<-md
+  names(md3)[2]<-'FeATURE'
+  v<-makeMetaVertex('FeATURE',md3)
+  expect_s3_class(v,'data.frame')
+  expect_equal(dim(v),c(4,3))
+  expect_identical(v,
+                   data.frame(ID=c('cFeature1','cFeature2','cFeature3','cFeature4'),
+                              Name=c('A1','A2','B1','B2'),
+                              Type=c('cFeature','cFeature','cFeature','cFeature'),
+                              stringsAsFactors = FALSE))
+  md4<-md
+  names(md4)[2]<-'feaTure'
+  v<-makeMetaVertex('feaTure',md4)
+  expect_s3_class(v,'data.frame')
+  expect_equal(dim(v),c(4,3))
+  expect_identical(v,
+                   data.frame(ID=c('cFeature1','cFeature2','cFeature3','cFeature4'),
+                              Name=c('A1','A2','B1','B2'),
+                              Type=c('cFeature','cFeature','cFeature','cFeature'),
+                              stringsAsFactors = FALSE))
 })
 
 test_that('makeMetaEdges makes proper edge table',{
@@ -40,7 +93,7 @@ test_that('makeMetaEdges makes proper edge table',{
   expect_identical(e,
                    data.frame(Source=c('class1','class1','class2','class2'),
                               Target=c('subclass1','subclass2','subclass3','subclass4'),
-                              value=rep(1e3,4)))
+                              value=rep(1e3,4),stringsAsFactors = FALSE))
 })
 
 test_that('makeMetaEdges gives warning when outside of boundary',{
@@ -56,4 +109,16 @@ test_that('makeMetanode makes graph with order',{
   expect_equal(length(l),2)
   expect_equal(dim(l$vertex),c(14,3))
   expect_equal(dim(l$edges),c(12,3))
+})
+
+test_that('makeGraph makes proper graph',{
+  fm<-fm[sample.int(dim(fm)[1]),] # for proper testing of md/fm matching
+  g<-makeGraph(featureMatrix=fm,
+                  metadata=md,
+                  colOrder=c('class','subclass','sample'),
+                  metaStrength = 1e3)
+  expect_type(g,'list')
+  expect_s3_class(g,'igraph')
+  expect_equal(vcount(g),39)
+  expect_equal(ecount(g),212)
 })
